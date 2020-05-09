53ab51276156ffc04b21dd662f9f504715bd47fe
robertarfa <roberta_rfa@hotmail.com>
2020-05-08 21:32:40 -0300

    admin almost done, missing index

--- public/script.js
+++ public/script.js
@@ -1,11 +1,15 @@
 const cards = document.querySelectorAll('.card');
+const addIngredient = document.querySelector('button.add-ingredient')
+const addStep = document.querySelector('button.add-step')
 
-for (let [index, card] of cards.entries()){
+for (const card of cards) {
 
-    card.addEventListener('click', function(){
-       window.location.href = `/recipes/${index}`
-              
-    })
+    if (!card.classList.contains('admin')) {
+        card.addEventListener('click', () => {
+            const index = card.getAttribute('id')
+            window.location.href = `/recipes/${index}`
+        })
+    }
 }
 
 const details = document.querySelectorAll('.details')
@@ -25,3 +29,25 @@ for (const detail of details) {
 
     console.log(a)
 }
+
+if (addIngredient)
+    addIngredient.addEventListener('click', () => {
+        const lastIngredient = document.querySelector('input[name="ingredients[]"]:last-child').cloneNode(true)
+        if (lastIngredient.value == '') {
+            alert('Preencha o último ingrediente!')
+            return
+        }
+        lastIngredient.value = ''
+        document.querySelector('.ingredients').appendChild(lastIngredient)
+    })
+
+if (addStep)
+    addStep.addEventListener('click', () => {
+        const lastStep = document.querySelector('input[name="preparation[]"]:last-child').cloneNode(true)
+        if (lastStep.value == '') {
+            alert('Preencha o último passo!')
+            return
+        }
+        lastStep.value = ''
+        document.querySelector('.preparation').appendChild(lastStep)
+    })
--- routes.js
+++ routes.js
@@ -1,32 +1,42 @@
 const express = require('express')
 const routes = express.Router()
-const recipe = require('./controllers/recipe')
-const recipes = require('./data')
+const data = require('./data.json')
+const recipes = require('./controllers/admRecipe')
 
-routes.get('/', function(req, res){
-    return res.render('index', {data: recipes})
+routes.get('/', function (req, res) {
+    res.render('index', { recipes: data.recipes })
 })
 
-routes.get('/about', function(req, res){
-    return res.render('about')
+routes.get('/about', function (req, res) {
+    res.render('about')
 })
 
-routes.get('/recipes-page', function(req, res){
-    return res.render('recipes-page',  { recipes })
+routes.get('/recipes-page', function (req, res) {
+    res.render('recipes-page', { recipes: data.recipes })
 })
 
-routes.get("/recipes/:index", function (req, res){
-    const index = req.params.index
-    return res.render("recipes", {data: recipes, index})
+routes.get('/recipes/:id', function (req, res) {
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function (recipe) {
+        return recipe.id == id
+    })
+
+    if (!foundRecipe) return res.send('Receita não encontrada!')
+
+    res.render('recipes', { recipe: foundRecipe })
 })
 
-//routes.get("/admin/recipes", recipe.index); // Mostrar a lista de receitas
-routes.get("/admin/recipes/create", recipe.create); // Mostrar formulário de nova receita
-routes.get("/admin/recipes/:index", recipe.show); // Exibir detalhes de uma receita
-routes.get("/admin/recipes/:index/edit", recipes.edit); // Mostrar formulário de edição de receita
+routes.get("/admin", function (req, res) {
+    res.redirect('/admin/recipes')
+})
 
-// routes.post("/admin/recipes", recipe.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", recipe.put); // Editar uma receita
-// routes.delete("/admin/recipes", recipe.delete); // Deletar uma receita
+routes.get("/admin/recipes", recipes.index) // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create) // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show) // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit) // Mostrar formulário de edição de receita
+routes.post("/admin/recipes", recipes.post) // Cadastrar nova receita
+routes.put("/admin/recipes", recipes.put) // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete) // Deletar uma receita
 
 module.exports = routes
